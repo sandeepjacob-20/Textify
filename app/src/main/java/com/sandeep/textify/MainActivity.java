e748855ab43388af986aa0d6a1b09f25563865f8
Sandeep Jacob <sandeepjacob1@gmail.com>
2022-06-16 20:05:11 +0530

    Initial commit

---
+++ app/src/main/java/com/sandeep/textify/MainActivity.java
@@ -0,0 +1,162 @@
+package com.sandeep.textify;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.huawei.hmf.tasks.OnFailureListener;
+import com.huawei.hmf.tasks.OnSuccessListener;
+import com.huawei.hmf.tasks.Task;
+
+import com.huawei.hms.mlsdk.MLAnalyzerFactory;
+import com.huawei.hms.mlsdk.common.MLFrame;
+import com.huawei.hms.mlsdk.text.MLLocalTextSetting;
+import com.huawei.hms.mlsdk.text.MLText;
+import com.huawei.hms.mlsdk.text.MLTextAnalyzer;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class MainActivity extends AppCompatActivity {
+    private Context context;
+    private Button capture;
+    private static final int request_camera_code = 100;
+    private static final int request_write_storage_code = 101;
+    private static final int request_read_storage_code = 102;
+    private Bitmap bitmap;
+    private String value;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        context = this;
+        capture = findViewById(R.id.capture);
+
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{
+                    Manifest.permission.CAMERA
+            },request_camera_code);
+        }
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            },request_write_storage_code);
+        }
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            },request_read_storage_code);
+        }
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.with(MainActivity.this)
+                        .crop()	    			//Crop image(Optional), Check Customization for more option
+                        .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                        .start();
+            }
+        });
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            Uri uri =  data.getData();
+            //Image Uri will not be null for RESULT_OK
+            //val uri: Uri = data?.data!!
+
+                    // Use Uri object instead of File to avoid storage permissions
+                   // imgProfile.setImageURI(fileUri)
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),uri);
+                recognize(bitmap);
+                //result.setText(value);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (resultCode == ImagePicker.RESULT_ERROR) {
+            Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show();
+        }
+        /*if (requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if(resultCode == RESULT_OK){
+                Uri resultUri = result.getUri();*/
+
+
+    }
+
+    private void recognize(Bitmap bitmap){
+        final String[] val = new String[1];
+        MLLocalTextSetting setting = new MLLocalTextSetting.Factory()
+                .setOCRMode(MLLocalTextSetting.OCR_DETECT_MODE)
+                // Specify languages that can be recognized.
+                .setLanguage("en")
+                .create();
+        MLTextAnalyzer analyzer = MLAnalyzerFactory.getInstance().getLocalTextAnalyzer(setting);
+        //MLTextAnalyzer analyzer = new MLTextAnalyzer.Factory(MainActivity.this).create();
+        //create a bit map image first
+        MLFrame frame = MLFrame.fromBitmap(bitmap);
+
+        Task<MLText> task = analyzer.asyncAnalyseFrame(frame);
+        task.addOnSuccessListener(new OnSuccessListener<MLText>() {
+            @Override
+            public void onSuccess(MLText text) {
+                // Processing for successful recognition.
+                value = text.getStringValue();
+                Intent intent = new Intent(MainActivity.this,ResultActivity.class);
+                intent.putExtra("result",value);
+                startActivity(intent);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(Exception e) {
+                Toast.makeText(MainActivity.this, "Couldn't recognise text", Toast.LENGTH_SHORT).show();
+                // Processing logic for recognition failure.
+            }
+        });
+
+        try {
+            if (analyzer != null) {
+                analyzer.stop();
+            }
+        } catch (IOException e) {
+            // Exception handling.
+        }
+
+    }
+}
+
---
+++ app/src/main/java/com/sandeep/textify/ResultActivity.java
@@ -0,0 +1,84 @@
+package com.sandeep.textify;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ResultActivity extends AppCompatActivity {
+    private Button save;
+    private TextView result;
+    private String value;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        save = findViewById(R.id.save);
+        result = findViewById(R.id.result);
+
+        value = getIntent().getExtras().getString("result");
+
+        result.setText(value);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAndSaveFile();
+            }
+        });
+    }
+
+    private void createAndSaveFile() {
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TITLE,"new_doc.txt");
+        someActivityResultLauncher.launch(intent);
+        //startActivityForResult(intent,1);
+    }
+
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent data = result.getData();
+                        Uri uri = data.getData();
+
+                        try {
+                            OutputStream outputStream = getContentResolver().openOutputStream(uri);
+                            outputStream.write(value.getBytes());
+                            outputStream.close();
+
+                            Toast.makeText(getApplicationContext(),"File Saved",Toast.LENGTH_LONG).show();
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(),"File Not Saved",Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+
+}
