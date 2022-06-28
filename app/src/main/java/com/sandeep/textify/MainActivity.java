3ce20a7d046b2d10ff63d49ea7f792e960707a56
Sandeep Jacob <sandeepjacob1@gmail.com>
2022-06-29 00:17:39 +0530

    added background, refined the UI

--- app/src/main/java/com/sandeep/textify/MainActivity.java
+++ app/src/main/java/com/sandeep/textify/MainActivity.java
@@ -42,58 +42,54 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     private void silentSignInByHwId() {
-        // 1. Use AccountAuthParams to specify the user information to be obtained, including the user ID (OpenID and UnionID), email address, and profile (nickname and picture).
-        // 2. By default, DEFAULT_AUTH_REQUEST_PARAM specifies two items to be obtained, that is, the user ID and profile.
-        // 3. If your app needs to obtain the user's email address, call setEmail().
-        //  4. To support ID token-based HUAWEI ID sign-in, use setIdToken(). User information can be parsed from the ID token.
-        mAuthParam = new AccountAuthParamsHelper(AccountAuthParams.DEFAULT_AUTH_REQUEST_PARAM)
-                .setEmail()
-                .setIdToken()
-                .createParams();
-        // Use AccountAuthParams to build AccountAuthService.
-        mAuthService = AccountAuthManager.getService(this, mAuthParam);
-        // Sign in with a HUAWEI ID silently.
-        Task<AuthAccount> task = mAuthService.silentSignIn();
-        task.addOnSuccessListener(new OnSuccessListener<AuthAccount>() {
-            @Override
-            public void onSuccess(AuthAccount authAccount) {
-                // The silent sign-in is successful. Process the returned AuthAccount object to obtain the HUAWEI ID information.
-                dealWithResultOfSignIn(authAccount);
-            }
-        });
-        task.addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(Exception e) {
-                // The silent sign-in fails. Your app will call getSignInIntent() to show the authorization or sign-in screen.
-                if (e instanceof ApiException) {
-                    ApiException apiException = (ApiException) e;
-                    Intent signInIntent = mAuthService.getSignInIntent();
-                    // If your app appears in full screen mode when a user tries to sign in, that is, with no status bar at the top of the device screen, add the following parameter in the intent:
-                    // intent.putExtra(CommonConstant.RequestParams.IS_FULL_SCREEN, true);
-                    // Check the details in this FAQ.
-                    signInIntent.putExtra(CommonConstant.RequestParams.IS_FULL_SCREEN, true);
-                    startActivityForResult(signInIntent, REQUEST_CODE_SIGN_IN);
+            //  1. Use AccountAuthParams to specify the user information to be obtained after user authorization, including the user ID (OpenID and UnionID), email address, and profile (nickname and picture).
+            // 2. By default, DEFAULT_AUTH_REQUEST_PARAM specifies two items to be obtained, that is, the user ID and profile.
+            // 3. If your app needs to obtain the user's email address, call setEmail().
+            // 4. To support authorization code-based HUAWEI ID sign-in, use setAuthorizationCode(). All user information that your app is authorized to access can be obtained through the relevant API provided by the Account Kit server.
+            mAuthParam = new AccountAuthParamsHelper(AccountAuthParams.DEFAULT_AUTH_REQUEST_PARAM)
+                    .setEmail()
+                    .setAuthorizationCode()
+                    .createParams();
+
+            // Use AccountAuthParams to build AccountAuthService.
+            mAuthService = AccountAuthManager.getService(this, mAuthParam);
+
+            // Sign in with a HUAWEI ID silently.
+            Task<AuthAccount> task = mAuthService.silentSignIn();
+            task.addOnSuccessListener(new OnSuccessListener<AuthAccount>() {
+                @Override
+                public void onSuccess(AuthAccount authAccount) {
+                    // The silent sign-in is successful. Process the returned AuthAccount object to obtain the HUAWEI ID information.
+                    dealWithResultOfSignIn(authAccount);
                 }
-            }
-        });
-    }
+            });
+            task.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(Exception e) {
+                    // The silent sign-in fails. Your app will call getSignInIntent() to show the authorization or sign-in screen.
+                    if (e instanceof ApiException) {
+                        ApiException apiException = (ApiException) e;
+                        Intent signInIntent = mAuthService.getSignInIntent();
+                        // If your app appears in full screen mode when a user tries to sign in, that is, with no status bar at the top of the device screen, add the following parameter in the intent:
+                        // intent.putExtra(CommonConstant.RequestParams.IS_FULL_SCREEN, true);
+                        // Check the details in this FAQ.
+                        signInIntent.putExtra(CommonConstant.RequestParams.IS_FULL_SCREEN, true);
+                        startActivityForResult(signInIntent, REQUEST_CODE_SIGN_IN);
+                    }
+                }
+            });
+        }
 
-    /**
-     * Process the returned AuthAccount object to obtain the HUAWEI ID information.
-     *
-     * @param authAccount AuthAccount object, which contains the HUAWEI ID information.
-     */
-    private void dealWithResultOfSignIn(AuthAccount authAccount) {
-        Log.i(TAG, "idToken:" + authAccount.getIdToken());
-        // TODO: After obtaining the ID token, your app will send it to your app server if there is one. If you have no app server, your app will verify and parse the ID token locally.
-        String access = authAccount.getAccessToken();
-        Intent intent = new Intent(getApplicationContext(),MainMenu.class);
-        intent.putExtra("name",authAccount.getDisplayName());
-        overridePendingTransition(R.anim.slide_to_left,R.anim.slide_from_right);
-        startActivity(intent);
-
-    }
+        private void dealWithResultOfSignIn(AuthAccount authAccount) {
+            Log.i(TAG,"code:"+authAccount.getAuthorizationCode());
+            // TODO: After obtaining the authorization code, your app needs to send it to the app server.
+            Intent intent = new Intent(getApplicationContext(),MainMenu.class);
+            intent.putExtra("name",authAccount.getDisplayName());
+            overridePendingTransition(R.anim.slide_to_left,R.anim.slide_from_right);
+            startActivity(intent);
+        }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -106,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
                 dealWithResultOfSignIn(authAccount);
                 Log.i(TAG, "onActivitResult of sigInInIntent, request code: " + REQUEST_CODE_SIGN_IN);
             } else {
-                // The sign-in fails. Find the cause from the status code. For more information, please refer to Error Codes.
+                // The sign-in fails. Find the failure cause from the status code. For more information, please refer to Error Codes.
                 Log.e(TAG, "sign in failed : " +((ApiException)authAccountTask.getException()).getStatusCode());
             }
         }
