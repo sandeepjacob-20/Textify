c3d5e971bed84406f1634411e426358ab5b9b663
Sandeep Jacob <sandeepjacob1@gmail.com>
2022-06-29 00:59:26 +0530

    added dummy ads to the result screen

--- app/src/main/java/com/sandeep/textify/ResultActivity.java
+++ app/src/main/java/com/sandeep/textify/ResultActivity.java
@@ -11,23 +11,44 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.huawei.hms.ads.AdListener;
+import com.huawei.hms.ads.AdParam;
+import com.huawei.hms.ads.banner.BannerView;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Locale;
 
 public class ResultActivity extends AppCompatActivity {
+    private static final String TAG = "destroyed";
     private Button save;
     private TextView result;
     private String value;
+
+    private BannerView bannerView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        bannerView = findViewById(R.id.hw_banner_view);
+        bannerView.setAdId("testw6vs28auh3");
+        bannerView.setBannerRefresh(60);
+        AdParam adParam = new AdParam.Builder().build();
+        bannerView.loadAd(adParam);
+        bannerView.setAdListener(adListener);
+
+
 
         save = findViewById(R.id.save);
         result = findViewById(R.id.result);
@@ -44,6 +65,44 @@ public class ResultActivity extends AppCompatActivity {
             }
         });
     }
+    private AdListener adListener = new AdListener() {
+        @Override
+        public void onAdLoaded() {
+            // Called when an ad is loaded successfully.
+            Toast.makeText(getApplicationContext(),"Ad loaded.",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onAdFailed(int errorCode) {
+            // Called when an ad fails to be loaded.
+            Toast.makeText(getApplicationContext(),String.format(Locale.ROOT, "Ad failed to load with error code %d.",errorCode),Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onAdOpened() {
+            // Called when an ad is opened.
+            Toast.makeText(getApplicationContext(),String.format("Ad opened "),Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onAdClicked() {
+            // Called when a user taps an ad.
+            Toast.makeText(getApplicationContext(),"Ad clicked",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onAdLeave() {
+            // Called when a user has left the app.
+            Toast.makeText(getApplicationContext(),"Ad Leave",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onAdClosed() {
+            // Called when an ad is closed.
+            Toast.makeText(getApplicationContext(),"Ad closed",Toast.LENGTH_SHORT).show();
+        }
+    };
+
 
     private void createAndSaveFile() {
         Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
@@ -80,5 +139,13 @@ public class ResultActivity extends AppCompatActivity {
                     }
                 }
             });
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+        if (bannerView != null) {
+            bannerView.destroy();
+        }
+    }
 
 }
